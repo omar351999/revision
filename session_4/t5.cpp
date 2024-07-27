1c7a729695a903d97d27ff308728774a0d1485db
Omar_Khaled <97206092+omar351999@users.noreply.github.com>
2024-07-27 16:19:45 +0300

    Delete session_4 directory

--- session_4/t5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	char arr[11] = {'m', 'K', 'r', 'a', 'O', 'l', 'h', 'a', ' ', 'e', 'd'};
-
-	cout << arr[4]<< arr[0]<< arr[3]<< arr[2]<< arr[8];
-	cout << arr[1]<< arr[6]<< arr[7]<< arr[5]<< arr[9]<< arr[10];
-}
